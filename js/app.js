894ea6af469b65e1f8e5e6ddaef5d625e7ef0296
syedzubairahmed001 <syedzubairahmed001@gmail.com>
2019-12-07 19:05:42 +0530

    added responsiveness and added nav to app page

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@ const video = document.getElementById('video')
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  // // faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  // faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  // faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(startVideo)
 
 function startVideo() {
